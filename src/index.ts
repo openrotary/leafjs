f94f75c2d85df1887a0602365a2269d949790da8
songjunjie <songjunjie@ygsoft.com>
2020-01-08 17:29:28 +0800

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { getRandom } from './utils'
+import { getRandom, log } from './utils'
 
 export default class Leaf {
     elementList: object[] = []
@@ -7,15 +7,65 @@ export default class Leaf {
             this.elementList = []
         }
     }
-    appendNode(_data: any, fb?: any): object[] {
-        if (!Array.isArray(_data)) {
-            // 对象
-            _data._mid = getRandom()
-        }
+    appendRootNode(_data: any, fb?: any): object[] {
         // 执行 before 钩子函数
+        const arr = this.elementList.concat()
+        console.log(arr)
         const data = fb ? fb(_data) : _data
-        this.elementList.push(data)
+        const _index = this.elementList.filter((item: any) => !item._pid).length
+        this.elementList.push({
+            _mid: getRandom(),
+            _index,
+            ...data
+        })
         return this.elementList
+    }
+    appendNode(mid: string, n: number, data: any): object[] {
+        // 执行 before 钩子函数
+        // log(data, '新元素')
+        if (n === 1) {
+            // 在相对元素上方追加
+            // 查看有多少兄弟元素
+            const [node]: any[] = this.elementList.filter((item: any) => item._mid === mid)
+            this.elementList.forEach((item: any) => {
+                if (item._pid && item._pid === node._pid && item._index >= node._index) {
+                    item._index++
+                }
+            })
+            this.elementList.push({
+                ...data,
+                _pid: node._pid,
+                _index: node._index,
+                _mid: data._mid || getRandom()
+            })
+        }
+        if (n === 2) {
+            // 在相对元素后追加
+            const _index = this.elementList.filter((item: any) => item._pid === mid).length
+            this.elementList.push({
+                ...data,
+                _pid: mid,
+                _index,
+                _mid: data._mid || getRandom()
+            })
+        }
+        if (n === 3) {
+            // 在相对元素下方追加
+            // 查看有多少兄弟元素
+            const [node]: any[] = this.elementList.filter((item: any) => item._mid === mid)
+            this.elementList.forEach((item: any) => {
+                if (item._pid && item._pid === node._pid && item._index >= node._index + 1) {
+                    item._index++
+                }
+            })
+            this.elementList.push({
+                ...data,
+                _pid: node._pid,
+                _index: node._index + 1,
+                _mid: data._mid || getRandom()
+            })
+        }
+        return this.elementList.map(({ children, ...item }: any) => item)
     }
     static data2tree(data: any[]) {
         // 二维数组转树形结构
@@ -38,14 +88,14 @@ export default class Leaf {
             }
             for (var j = 0, max = res.length; j < max; j++) {
                 var item = res[j]
-                item.children = array2Tree(arrCopy, item._mid)
+                item.children = array2Tree(arrCopy, item._mid).sort((a, b) => a._index - b._index)
             }
             return res
         }
         return _arr.map(({ _mid, ...item }) => ({
             ...item,
             _mid,
-            children: array2Tree(data, _mid)
+            children: array2Tree(data, _mid).sort((a, b) => a._index - b._index)
         }))
     }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,12 @@ export const getRandom = (min?: number, max?: number): string => {
     }
     return String(Math.round(Math.random() * (max - min) + min))
 }
+
+export const log = (data, msg = '打印') => {
+    console.log(
+        `%c ${msg} %c \n`,
+        'background-color: #82ae46; color: #fff;padding:3px;box-sizing: border-box;border-radius: 3px;',
+        '',
+        data
+    )
+}
